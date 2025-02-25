272119a8cf0ffd4c1e5b62a63a0e0b6355f175a3
Kamioda <kamiodamasato@outlook.com>
2025-02-25 22:29:05 +0900

    change package, alias and arg name

--- src/bin.ts
+++ src/bin.ts
@@ -5,13 +5,13 @@ import { writeJson } from 'nodeeasyfileio';
 import PostCodeDataLoader from './index';
 
 const options = commandLineArgs([
-    { name: 'init', alias: 'i', type: Boolean },
+    { name: 'init-package', alias: 'i', type: Boolean },
     { name: 'master-csv-path', alias: 'm', type: String, defaultValue: 'KEN_ALL.CSV' },
     { name: 'search-postcode', alias: 'p', type: Boolean },
     { name: 'postcode', type: String },
 ]);
 
-if (options['init']) {
+if (options['init-package']) {
     if (!options['master-csv-path']) throw new Error('master-csv-path is required');
     const MasterCSV = readDataFromCSV(options['master-csv-path']);
     const Prefectures = MasterCSV.map(i => i[6]).filter((x, i, self) => self.indexOf(x) === i);
