02cb94ba426c9abe121a2ba29216c0885a48e27b
Kamioda <kamiodamasato@outlook.com>
2025-03-01 17:04:03 +0900

    bug fix: crash when directory './postcodes/patch' is not found

--- src/worker.ts
+++ src/worker.ts
@@ -1,4 +1,4 @@
-import { readdirSync, statSync } from 'fs';
+import { existsSync, readdirSync, statSync } from 'fs';
 import { readDataFromCSV, readDataFromJson } from './read';
 import { PostCodeRecord } from './PostCodeRecord';
 
@@ -6,38 +6,40 @@ export default class Worker {
     private Record: PostCodeRecord<number, string>[];
     constructor(prefecture: string) {
         this.Record = readDataFromJson(`./postcodes/master/${prefecture}.json`);
-        readdirSync(`./postcodes/patch`)
-            .map(dir => `./postcodes/patch/${dir}`)
-            .filter(i => statSync(i).isDirectory())
-            .sort()
-            .forEach(dir => {
-                const AddRecordReadResult = readDataFromCSV(dir + '/ADD.CSV').filter(i => i.prefecture === prefecture);
-                const DelRecordReadResult = readDataFromCSV(dir + '/DEL.CSV').filter(i => i.prefecture === prefecture);
-                const data = {
-                    new: AddRecordReadResult.filter(
-                        i => DelRecordReadResult.findIndex(j => j.postcode === i.postcode) === -1
-                    ),
-                    del: DelRecordReadResult.filter(
-                        i => AddRecordReadResult.findIndex(j => j.postcode === i.postcode) === -1
-                    ),
-                    update: AddRecordReadResult.filter(
-                        i => DelRecordReadResult.findIndex(j => j.postcode === i.postcode) !== -1
-                    ),
-                };
-                data.new.forEach(i => {
-                    this.Record.push(i);
-                });
-                data.del
-                    .filter(i => i.prefecture === prefecture)
-                    .forEach(i => {
-                        const index = this.Record.findIndex(j => j.postcode === i.postcode);
-                        if (index !== -1) this.Record.splice(index, 1);
+        if (existsSync(`./postcodes/patch`) && statSync(`./postcodes/patch`).isDirectory()) {
+            readdirSync(`./postcodes/patch`)
+                .map(dir => `./postcodes/patch/${dir}`)
+                .filter(i => statSync(i).isDirectory())
+                .sort()
+                .forEach(dir => {
+                    const AddRecordReadResult = readDataFromCSV(dir + '/ADD.CSV').filter(i => i.prefecture === prefecture);
+                    const DelRecordReadResult = readDataFromCSV(dir + '/DEL.CSV').filter(i => i.prefecture === prefecture);
+                    const data = {
+                        new: AddRecordReadResult.filter(
+                            i => DelRecordReadResult.findIndex(j => j.postcode === i.postcode) === -1
+                        ),
+                        del: DelRecordReadResult.filter(
+                            i => AddRecordReadResult.findIndex(j => j.postcode === i.postcode) === -1
+                        ),
+                        update: AddRecordReadResult.filter(
+                            i => DelRecordReadResult.findIndex(j => j.postcode === i.postcode) !== -1
+                        ),
+                    };
+                    data.new.forEach(i => {
+                        this.Record.push(i);
                     });
-                data.update.forEach(i => {
-                    const index = this.Record.findIndex(j => j.postcode === i.postcode);
-                    if (index !== -1) this.Record[index] = i;
+                    data.del
+                        .filter(i => i.prefecture === prefecture)
+                        .forEach(i => {
+                            const index = this.Record.findIndex(j => j.postcode === i.postcode);
+                            if (index !== -1) this.Record.splice(index, 1);
+                        });
+                    data.update.forEach(i => {
+                        const index = this.Record.findIndex(j => j.postcode === i.postcode);
+                        if (index !== -1) this.Record[index] = i;
+                    });
                 });
-            });
+        }
         this.Record.sort((a, b) => a.id - b.id);
     }
     private InternalGetPostCodeInformation(
